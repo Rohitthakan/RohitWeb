1303346cda685994df9032f2319653155b175c4f
Rohitthakan <110251143+Rohitthakan@users.noreply.github.com>
2024-06-18 13:34:20 +0530

    Update settings.py

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -177,11 +177,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # EMAIL CONFIGURATION (for contact form)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'collegerepo2023@gmail.com'
-EMAIL_HOST_PASSWORD = 'vyjb prco cmbg fqqq'
+EMAIL_HOST_USER = 'your example_email'
+EMAIL_HOST_PASSWORD = 'your example_email app password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_PASSWORD = 'hfqjknkonjknovjknkjljglydkbkjjej'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
