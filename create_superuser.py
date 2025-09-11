52832a12de0ed35ef8ad8fd32802bd8cbee96d47
Rohitthakan <110251143+Rohitthakan@users.noreply.github.com>
2025-09-12 01:06:45 +0530

    Add files via upload

---
+++ create_superuser.py
@@ -0,0 +1,17 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourproject.settings")
+django.setup()
+
+from django.contrib.auth.models import User
+
+username = "rohit"
+email = ""
+password = "Thakan@123"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created ✅")
+else:
+    print("Superuser already exists 🚀")
