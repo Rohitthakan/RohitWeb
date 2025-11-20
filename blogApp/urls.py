3c8109733b117979d5435b56ff05a6ea7a440e57
Rohitthakan <110251143+Rohitthakan@users.noreply.github.com>
2025-11-20 11:51:42 +0530

    Update urls.py

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -11,8 +11,8 @@ from home.views import backup_db_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path('backup-db/', backup_db_view, name='backup-db'),
     path('', include('home.urls')),
-    path('backup-db/', backup_db_view, name='backup-db')
     # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
